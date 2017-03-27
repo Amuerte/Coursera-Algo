8ff98bf4714e9a23e126a2ea43e0f901ff9fb67a
Amuerte <amuerte@yopmail.com>
2017-03-27 10:56:59 +0100

    Create week1 exam - Percolation
    	modified:   .gitignore
    	new file:   checkstyle-coursera.xml
    	new file:   findbugs-coursera.xml
    	new file:   percolation/Percolation.java
    	new file:   percolation/PercolationStats.java

---
+++ percolation/Percolation.java
@@ -0,0 +1,74 @@
+
+public class Percolation {
+	
+	
+	/**
+	 * Create n-by-n grid, with all sites blocked
+	 * 
+	 * @param n
+	 */
+	public Percolation(int n) {
+
+	}
+
+	/**
+	 * open site (row, col) if it is not open already
+	 * 
+	 * @param row
+	 * @param col
+	 */
+	public void open(int row, int col) {
+
+	}
+
+	/**
+	 * is site (row, col) open.
+	 * 
+	 * @param row
+	 * @param col
+	 * @return
+	 */
+	public boolean isOpen(int row, int col) {
+
+		return true;
+	}
+
+	/**
+	 * is site (row, col) full?
+	 * 
+	 * @param row
+	 * @param col
+	 * @return
+	 */
+	public boolean isFull(int row, int col) {
+		
+		return true;
+	}
+
+	/**
+	 * Number of open sites
+	 * 
+	 * @return
+	 */
+	public int numberOfOpenSites() {
+		return 0;
+	}
+
+	/**
+	 * Does the system percolate
+	 * 
+	 * @return
+	 */
+	public boolean percolates() {
+		return false;
+	}
+
+	/**
+	 * test client (optional)
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+}
---
+++ percolation/PercolationStats.java
@@ -0,0 +1,59 @@
+
+public class PercolationStats {
+
+	/**
+	 * Perform trials independent experiments on an n-by-n grid
+	 * 
+	 * @param n
+	 * @param trials
+	 */
+	public PercolationStats(int n, int trials) {
+
+	}
+
+	/**
+	 * Sample mean of percolation threshold
+	 * 
+	 * @return
+	 */
+	public double mean() {
+		return 0;
+	}
+
+	/**
+	 * Sample standard deviation of percolation threshold
+	 * 
+	 * @return
+	 */
+	public double stddev() {
+		return 0;
+	}
+
+	/**
+	 * Low endpoint of 95% confidence interval
+	 * 
+	 * @return
+	 */
+	public double confidenceLo() {
+		return 0;
+	}
+
+	/**
+	 * High endpoint of 95% confidence interval
+	 * 
+	 * @return
+	 */
+	public double confidenceHi() {
+		return 0;
+	}
+
+	/**
+	 * test client
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
